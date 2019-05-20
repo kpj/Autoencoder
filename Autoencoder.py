91d66fd4b951b7c2f41bb58a3d4dc0b25660e9ec
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2019-05-20 10:11:23 +0200

    Add working example

---
+++ Autoencoder.py
@@ -0,0 +1,214 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from tqdm.auto import tqdm, trange
+
+# +
+tfd = tfp.distributions
+
+plt.set_cmap('gray')
+sns.set_context('talk')
+# -
+
+# # Glossary
+
+# * epoch: one cycle through training data (made up of many steps)
+# * step: one gradient update per batch of data
+# * learning rate: how fast to follow gradient (in gradient descent)
+
+# # Introduction
+
+# ## Basic operations
+
+# +
+a = tf.constant(2.)
+b = tf.constant(3.)
+
+print(tf.add(a, b))
+print(tf.reduce_mean([a, b]))
+
+# +
+m1 = tf.constant([[1., 2.], [3., 4.]])
+m2 = tf.constant([[5., 6.], [7., 8.]])
+
+tf.matmul(m1, m2)
+
+
+# -
+
+# # Basic Autoencoder
+
+# ## Loss function
+
+# $$
+# \frac{1}{N} \sum_{i=0}^N (\hat{x}_i - x_i)^2
+# $$
+
+def loss_func(model, original):
+    return tf.reduce_mean(
+        tf.square(
+            tf.subtract(
+                model(original),
+                original
+            )
+        )
+    )
+
+
+# ## Model
+
+# +
+class Encoder(tf.keras.layers.Layer):
+    """Convert input to low-dimensional representation."""
+
+    def __init__(self, intermediate_dim):
+        super(Encoder, self).__init__()
+        self.hidden_layer = tf.keras.layers.Dense(
+            units=intermediate_dim, activation=tf.nn.relu)
+        self.output_layer = tf.keras.layers.Dense(
+            units=intermediate_dim, activation=tf.nn.relu)
+
+    def call(self, input_features):
+        activation = self.hidden_layer(input_features)
+        return self.output_layer(activation)
+
+
+class Decoder(tf.keras.layers.Layer):
+    """Reconstruct input from low-dimensional representation."""
+
+    def __init__(self, intermediate_dim, original_dim):
+        super(Decoder, self).__init__()
+        self.hidden_layer = tf.keras.layers.Dense(
+            units=intermediate_dim, activation=tf.nn.relu)
+        self.output_layer = tf.keras.layers.Dense(
+            units=original_dim, activation=tf.nn.relu)
+
+    def call(self, code):
+        activation = self.hidden_layer(code)
+        return self.output_layer(activation)
+
+
+class Autoencoder(tf.keras.Model):
+    """Connect all components to single model."""
+
+    def __init__(self, intermediate_dim, original_dim):
+        super(Autoencoder, self).__init__()
+        
+        # setup architecture
+        self.encoder = Encoder(
+            intermediate_dim=intermediate_dim)
+        self.decoder = Decoder(
+            intermediate_dim=intermediate_dim, original_dim=original_dim)
+        
+        # helpful stuff
+        self.train_loss = tf.keras.metrics.Mean(name='train_loss')
+
+    def call(self, input_features):
+        code = self.encoder(input_features)
+        reconstructed = self.decoder(code)
+        return reconstructed
+    
+    @tf.function
+    def train_step(self, data, optimizer):
+        with tf.GradientTape() as tape:
+            loss = loss_func(self, data)
+            
+            gradients = tape.gradient(loss, self.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+            
+        self.train_loss(loss)
+
+
+# -
+
+# ## Analysis
+
+# ### Parameters
+
+np.random.seed(1)
+tf.random.set_seed(1)
+
+batch_size = 128
+epochs = 20
+learning_rate = 1e-3
+intermediate_dim = 64
+original_dim = 784
+
+# ### Load data
+
+# +
+(training_features, _), (test_features, _) = tf.keras.datasets.mnist.load_data()
+training_features = training_features / np.max(training_features)
+
+# flatten 2D images into 1D
+training_features = training_features.reshape(
+    training_features.shape[0],
+    training_features.shape[1] * training_features.shape[2]).astype(np.float32)
+
+training_dataset = tf.data.Dataset.from_tensor_slices(training_features).batch(batch_size)
+training_dataset
+# -
+
+# ### Train model
+
+autoencoder = Autoencoder(intermediate_dim=intermediate_dim, original_dim=original_dim)
+
+opt = tf.optimizers.Adam(learning_rate=learning_rate)
+
+loss_list = []
+for epoch in trange(epochs, desc='Epochs'):
+    for step, batch_features in enumerate(training_dataset):
+        autoencoder.train_step(batch_features, opt)
+        
+    loss_list.append(autoencoder.train_loss.result().numpy())
+
+# ### Investigate results
+
+plt.plot(loss_list)
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+
+inp = np.random.normal(size=64)
+plt.imshow(autoencoder.decoder([inp]).numpy()[0].reshape(28, 28))
+
+original = tf.reshape(
+    batch_features, (batch_features.shape[0], 28, 28))
+reconstructed = tf.reshape(
+    autoencoder(tf.constant(batch_features)),
+    (batch_features.shape[0], 28, 28))
+
+# +
+N = 2
+fig, ax = plt.subplots(nrows=N, ncols=2, figsize=(10, N*5))
+
+for i, ap in enumerate(ax):
+    ap[0].imshow(original.numpy()[i])
+    ap[1].imshow(reconstructed.numpy()[i])
+
+    if i == 0:
+        ap[0].set_title('Input')
+        ap[1].set_title('Output')
+
+    for a in ap:
+        a.set_xticks([])
+        a.set_yticks([])
---
+++ README.md
@@ -0,0 +1,7 @@
+# Fun with Autoencoders
+
+Various scripts for dealing with Autoencoders of different flavors.
+
+
+## Dev notes
+
